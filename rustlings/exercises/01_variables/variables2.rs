fe4de40dbece1a1caf7efafd839581b8bc2ba532
Yoyo Yuan <yoyoyuan1729@gmail.com>
2025-07-12 12:29:01 -0700

    one repo

---
+++ rustlings/exercises/01_variables/variables2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = 42; // explicit annotation
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
